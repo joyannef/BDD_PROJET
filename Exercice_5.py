9254e9dd6f66c8ecb68c2f3395bb4cfb59a1b74c
joyannef <95722762+joyannef@users.noreply.github.com>
2022-12-24 18:39:38 +0100

    Update Exercice_5.py

--- Exercice_5.py
+++ Exercice_5.py
@@ -1,1 +1,117 @@
+import sqlite3
+import csv
+
+#connect to the database
+connect = sqlite3.connect('/Users/jfoster/Documents/PluriTAL/BDD/test/bars.db')
+cursor = connect.cursor()
+
+#  l’identifiant du manager accédant aux informations.
+droits_utilisateurs = cursor.execute("""SELECT matricule
+                                        FROM employes
+                                        WHERE profession = 'Manager'
+                                        GROUP BY nom""")
+a = []
+b = []
+for item in droits_utilisateurs:
+    a.append(item)
+for i in a:
+    b.append(i[0])
+
+#Les matricules dans b sont des matricules des managers
+print (b)
+
+# Accès aux ventes effectuées dans son bar étant donné que la matricule est celle d'un manager
+in_b = True
+while in_b:
+    find_matricule = input("Entrer votre matricule: ")
+    if find_matricule in b:
+        print ("Vous avez l'accès à les informations suivantes...")
+        if find_matricule == "T80612":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Le Saphir"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "R25976":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "L'Envers Bodega"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "R81326":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "By Coss Bar"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "Q66872":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Nu-Bahia"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "Q09012":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Antirouille"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "O42298":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Comptoir de l'Arc"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "A08113":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Comptoir Saint-Paul"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "Q42796":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "La Barbote"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "R66525":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Chez Félix"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+        if find_matricule == "W04397":
+            vente_emp = cursor.execute("""SELECT nom_bar, prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule AND nom_bar = "Le Sens Six"
+                                        GROUP BY nom_bar, nom""")
+            res = cursor.fetchall()
+            for item in res:
+                print(item)
+    else:
+        print ("ERREUR. Vous n'avez pas d'accès à ces informations...")
+        break
+
+##FAUT AJOUTER INFO DES VENTES
+connect.commit()
+
+connect.close()
 
