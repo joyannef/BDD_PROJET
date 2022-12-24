3be71454db6cc0decffef252cb6c67177fe73e49
joyannef <95722762+joyannef@users.noreply.github.com>
2022-12-24 17:19:42 +0100

    Update Exercice_3.py

--- Exercice_3.py
+++ Exercice_3.py
@@ -1,1 +1,18 @@
+import sqlite3
+import csv
 
+#connect to the database
+connect = sqlite3.connect('/Users/jfoster/Documents/PluriTAL/BDD/test/bars.db')
+cursor = connect.cursor()
+
+#no. total de boissons vendues par chaque employé
+vente_emp = cursor.execute("""SELECT prenom, nom, matricule, COUNT (employe_id)
+                                        FROM employes, ventes
+                                        WHERE ventes.employe_id = employes.matricule
+                                        GROUP BY nom""")
+
+#manque montant total!!
+
+res = cursor.fetchall()
+for item in res:
+    print(item)
