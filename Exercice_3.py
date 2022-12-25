162d7253224110b0a36c65984727ffe83e747531
joyannef <95722762+joyannef@users.noreply.github.com>
2022-12-25 18:47:53 +0100

    Update Exercice_3.py

--- Exercice_3.py
+++ Exercice_3.py
@@ -11,8 +11,30 @@ vente_emp = cursor.execute("""SELECT prenom, nom, matricule, COUNT (employe_id)
                                         WHERE ventes.employe_id = employes.matricule
                                         GROUP BY nom""")
 
-#manque montant total!!
+resultat = cursor.fetchall()
+for item in resultat:
+    print(item)
 
+#Montant total vendue par employé
+montant_total_emp = cursor.execute(""" SELECT prenom, nom, employe_id, ROUND(S, 6) FROM (SELECT prenom, nom, employe_id, boisson_id, SUM(prix) AS S
+                                              FROM ventes, carte, employes
+                                              WHERE ventes.boisson_id = carte.id_Boisson AND ventes.employe_id = employes.matricule
+                                              GROUP BY employe_id)
+                                              ORDER BY S DESC
+                                             """)
 res = cursor.fetchall()
 for item in res:
     print(item)
+
+#     Liste de toutes les boissons vendues ainsi que le prix par employé
+# """SELECT employe_id, boisson_id, prix, date
+#                                               FROM ventes, carte
+#                                               WHERE ventes.boisson_id = carte.id_Boisson
+#                                      ORDER BY employe_id, boisson_id
+#                                              """)
+
+#ESSAYER FAIRE UN UNION DES RESULTATS
+
+connect.commit()
+
+connect.close()
