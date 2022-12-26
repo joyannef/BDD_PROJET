d197e76dd1225f40e1b4d156b7a3fa996fa9ec1e
joyannef <95722762+joyannef@users.noreply.github.com>
2022-12-26 15:29:13 +0100

    Update Exercice_BDD_12.py

--- Exercice_BDD_12.py
+++ Exercice_BDD_12.py
@@ -1,1 +1,41 @@
+import sqlite3
+import csv
+
+#connect to the database
+connect = sqlite3.connect('/Users/jfoster/Documents/PluriTAL/BDD/test/bars.db')
+cursor = connect.cursor()
+
+#Afficher les boissons les moins vendues
+boissons_vendues = cursor.execute("""SELECT nom_Boisson, boisson_id, COUNT(boisson_id) AS S
+                                              FROM ventes, carte
+                                              WHERE ventes.boisson_id = carte.id_Boisson
+                                              GROUP BY boisson_id
+                                              ORDER BY S ASC
+                                              LIMIT 3
+                                             """)
+
+resultat = cursor.fetchall()
+for item in resultat:
+    print(item)
+#Afficher les boissons qui ont rapporté le moins d'argent
+boissons_vendues = cursor.execute("""SELECT nom_Boisson, boisson_id, S FROM (SELECT nom_Boisson, boisson_id, SUM(prix) AS S
+                                              FROM ventes, carte
+                                              WHERE ventes.boisson_id = carte.id_Boisson
+                                              GROUP BY boisson_id
+                                              ORDER BY S ASC
+                                              LIMIT 3)
+                                             """)
+
+resultat = cursor.fetchall()
+for item in resultat:
+    print(item)
+
+
+connect.commit()
+
+connect.close()
+
+#Faut ajouter une ligne pour supprimer boissons 
+
+
 
